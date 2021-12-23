593a4fc8ee2b5ebb8ccd4d7702be2f395db1836d
ARochlitzer <72524586+ARochlitzer@users.noreply.github.com>
2021-12-24 00:48:19 +0100

    Update ListMetadata.vue

--- frontend/components/tasks/metadata/ListMetadata.vue
+++ frontend/components/tasks/metadata/ListMetadata.vue
@@ -10,7 +10,7 @@
   >
     <template #[`item.value`]="{ item }">
       <template v-if="item.key.indexOf('im_url') > -1">
-        <a :href="item.value" target="_blank"><img :src="item.value" style="height: 500px" /></a>
+        <a :href="item.value" target="_blank"><img :src="item.value" style="height: 1000px" /></a>
       </template>
       <template v-else-if="item.key.indexOf('url') > -1">
         <a :href="item.value" target="_blank">{{ item.value }}</a>
