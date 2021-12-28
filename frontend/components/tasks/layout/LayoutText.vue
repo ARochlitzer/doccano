82e971fe13847843144392392511ccce1bfb3e76
ARochlitzer <72524586+ARochlitzer@users.noreply.github.com>
2021-12-28 15:24:31 +0100

    Update LayoutText.vue

--- frontend/components/tasks/layout/LayoutText.vue
+++ frontend/components/tasks/layout/LayoutText.vue
@@ -2,11 +2,8 @@
   <v-container fluid>
     <slot name="header" />
     <v-row justify="center">
-      <v-col cols="12" md="9">
+      <v-col cols="12">
         <slot name="content" />
-      </v-col>
-      <v-col cols="12" md="3">
-        <slot name="sidebar" />
       </v-col>
     </v-row>
   </v-container>
